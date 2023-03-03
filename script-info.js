681bfde19e5d08d132c99f6b4af2ff1e2515f585
meikedve <115540545+meikedve@users.noreply.github.com>
2023-03-03 14:28:33 +0100

    dom manipulatie begin

--- script-info.js
+++ script-info.js
@@ -19,7 +19,7 @@ class Director extends Artist{
         this.movies = movies;
     }
 }
-class Writer extends Writer{
+class Writer extends Artist{
     constructor(name, birthyear, books){
         super(name, birthyear)
         this.books = books;
@@ -36,16 +36,17 @@ class Actor extends Artist{
 titanic = new Movie("Titanic", "Romance/Drama", 1998);
 director = new Director("James Cameron", 1954, ['Avatar', 'Avatar: The Way of Water', 'Terminator 2: Judgement Day']);
 writer = new Writer('James Cameron', 1954, ['no books written']);
-dicaprio = new Actor('Leonardo DiCaprio', 1974, ['The Wolf of Wallstreet', 'The Revenant', 'Romeo + Juliet'], character-photos/leonardo_dicaprio.jpg);
-winslet = new Actor('Kate Winslet', 1975, ['Avatar: The Way of Water', 'Mare of Easttown', 'The Holiday'], character-photos/kate_winslet.jpg);
-zane = new Actor('Billy Zane', 1966, ['The Phantom', 'Dead Calm', 'Back to the Future Part II'], character-photos/billy_zane.jpg);
-bates = new Actor('Kathy Bates', 1948, ['Misery', 'Dolores Claiborne', 'Fried Green Tomatoes'], character-photos/kathy_bates.jpg);
-fisher = new Actor('Frances Fisher', 1952, ['Holidate', 'Unforgiven', 'The Sinner'], character-photos/francis_fisher.jpg);
+dicaprio = new Actor('Leonardo DiCaprio', 1974, ['The Wolf of Wallstreet', 'The Revenant', 'Romeo + Juliet'], "character-photos/leonardo_dicaprio.jpg");
+winslet = new Actor('Kate Winslet', 1975, ['Avatar: The Way of Water', 'Mare of Easttown', 'The Holiday'], "character-photos/kate_winslet.jpg");
+zane = new Actor('Billy Zane', 1966, ['The Phantom', 'Dead Calm', 'Back to the Future Part II'], "character-photos/billy_zane.jpg");
+bates = new Actor('Kathy Bates', 1948, ['Misery', 'Dolores Claiborne', 'Fried Green Tomatoes'], "character-photos/kathy_bates.jpg");
+fisher = new Actor('Frances Fisher', 1952, ['Holidate', 'Unforgiven', 'The Sinner'], "character-photos/francis_fisher.jpg");
 
-var body = document.getElementsByTagName('body');
-var nav = document.getElementsByTagName('nav')
-var article = document.createElement('article');
-body.appendChild(article); 
+var main = document.getElementById('main');
 var title = document.createElement('p');
-var text = document.createTextNode('hoi')
-article.appendChild(text);
+title.setAttribute("id","title")
+title.append(titanic.title);
+main.appendChild(title);
+
+
+
