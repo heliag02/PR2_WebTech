9a0a2658d4cc13fe5cb26bbd5ce442844621ddd1
heliagh <heliaghasemi0@gmail.com>
2023-03-03 13:52:17 +0100

    mnbhjjh

--- script-info.js
+++ script-info.js
@@ -34,8 +34,18 @@ class Actor extends Artist{
     }
 }
 titanic = new Movie("Titanic", "Romance/Drama", 1998);
-director = new Director("James Cameron", 1954, ['Avatar', 'Avatar: The Way of Water', 'Terminator 2: Judgement Day'])
-writer = new Writer('James Cameron', 1954, ['no books written'])
-dicaprio = new Actor('Leonardo DiCaprio', 1974, ['The Wolf of Wallstreet', 'The Revenant', 'Romeo + Juliet'], character-photos/leonardo_dicaprio.jpg)
-winslet = new Actor('Kate Winslet')
+director = new Director("James Cameron", 1954, ['Avatar', 'Avatar: The Way of Water', 'Terminator 2: Judgement Day']);
+writer = new Writer('James Cameron', 1954, ['no books written']);
+dicaprio = new Actor('Leonardo DiCaprio', 1974, ['The Wolf of Wallstreet', 'The Revenant', 'Romeo + Juliet'], character-photos/leonardo_dicaprio.jpg);
+winslet = new Actor('Kate Winslet', 1975, ['Avatar: The Way of Water', 'Mare of Easttown', 'The Holiday'], character-photos/kate_winslet.jpg);
+zane = new Actor('Billy Zane', 1966, ['The Phantom', 'Dead Calm', 'Back to the Future Part II'], character-photos/billy_zane.jpg);
+bates = new Actor('Kathy Bates', 1948, ['Misery', 'Dolores Claiborne', 'Fried Green Tomatoes'], character-photos/kathy_bates.jpg);
+fisher = new Actor('Frances Fisher', 1952, ['Holidate', 'Unforgiven', 'The Sinner'], character-photos/francis_fisher.jpg);
 
+var body = document.getElementsByTagName('body');
+var nav = document.getElementsByTagName('nav')
+var article = document.createElement('article');
+body.appendChild(article); 
+var title = document.createElement('p');
+var text = document.createTextNode('hoi')
+article.appendChild(text);
