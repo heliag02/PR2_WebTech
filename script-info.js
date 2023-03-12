677e5c420238dbb9f30544c7969b729144babbe6
lminnebreuker <l.minnebreuker@students.uu.nl>
2023-03-12 17:06:50 +0100

    uitbreiding tooltip en aanpassingen nav feedback

--- script-info.js
+++ script-info.js
@@ -37,12 +37,22 @@ class Actor extends Artist{
 }
 titanic = new Movie("Titanic", "Romance/Drama", 1998,"A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.");
 cameron = new Director("James Cameron", 1954, ['Avatar', 'Avatar: The Way of Water', 'Terminator 2: Judgement Day']);
-//cameron = new Writer('James Cameron', 1954, []);
+landau = new Writer('Jon Landau', 1960, ['Solaris', 'Avatar', 'Dick Tracy']);
 dicaprio = new Actor('Leonardo DiCaprio', 1974, ['The Wolf of Wallstreet', 'The Revenant', 'Romeo + Juliet'], "character-photos/leonardo_dicaprio.jpg");
 winslet = new Actor('Kate Winslet', 1975, ['Avatar: The Way of Water', 'Mare of Easttown', 'The Holiday'], "character-photos/kate_winslet.jpg");
 zane = new Actor('Billy Zane', 1966, ['The Phantom', 'Dead Calm', 'Back to the Future Part II'], "character-photos/billy_zane.jpg");
 bates = new Actor('Kathy Bates', 1948, ['Misery', 'Dolores Claiborne', 'Fried Green Tomatoes'], "character-photos/kathy_bates.jpg");
 fisher = new Actor('Frances Fisher', 1952, ['Holidate', 'Unforgiven', 'The Sinner'], "character-photos/francis_fisher.jpg");
+
+function createListBooks(person){
+    var booksList = document.createElement('ul');
+    for (let index in person.books){
+        let listItem = document.createElement('li');
+        listItem.append(person.books[index]);
+        booksList.appendChild(listItem);
+    }
+    return booksList;
+}
 
 function createListMovies(person){
     var moviesList = document.createElement('ul');
@@ -93,16 +103,50 @@ for (let index in titanic){
 }
 
 //director info
-var directorSection = createSection(cameron.name + " (director/writer)");
+var directorSection = createSection("Director");
+var directorNamePar = document.createElement('p');
+directorNamePar.className = "tooltip";
+directorNamePar.id = cameron.name;
+
+var tooltip = document.createElement('span');
+tooltip.className = "tooltip__text";
+tooltip.id=cameron.name+"__tooltip";
+
 for (let index in cameron){
-    let value = cameron[index]
+    let par = document.createElement('p');
+    let value = cameron[index];
     if (index=='movies'){
         value = createListMovies(cameron);
     }
-    let par = document.createElement('p');
     par.append(createFillIn(index), value);
-    directorSection.appendChild(par);
+    tooltip.append(par);
 }
+
+directorNamePar.append(cameron.name,tooltip);
+directorSection.append(directorNamePar);
+
+//writer info
+var writerSection = createSection("Writer");
+var writerNamePar = document.createElement('p');
+writerNamePar.className = "tooltip";
+writerNamePar.id = landau.name;
+
+var tooltip2 = document.createElement('span');
+tooltip2.className = "tooltip__text";
+tooltip2.id=landau.name+"__tooltip";
+
+for (let index in landau){
+    let par = document.createElement('p');
+    let value = landau[index];
+    if (index=='books'){
+        value = createListBooks(landau);
+    }
+    par.append(createFillIn(index), value);
+    tooltip2.append(par);
+}
+
+writerNamePar.append(landau.name,tooltip2);
+writerSection.append(writerNamePar);
 
 //actors info
 var actors = [dicaprio,winslet,zane,bates,fisher];
@@ -120,7 +164,7 @@ for (let actor of actors) {
 
     for (let index in actor){
         let par = document.createElement('p');
-        let value = actor[index]
+        let value = actor[index];
         if (index=='photo'){
             break;
         }
@@ -162,7 +206,7 @@ linkTrailer.append('Go to the film trailer');
 
 linkPar.append(linkPoster,linkTrailer);
 linkSection.appendChild(linkPar);
-main.append(filmSection, directorSection,actorsSection,linkSection);
+main.append(filmSection, directorSection,writerSection,actorsSection,linkSection);
 
 const targets = document.getElementsByClassName("tooltip");
 for (let target of targets){
